25ee3d987422c9599f5638f80f8c9c723b63f49d
dima19972525 <dima19972525@gmail.com>
2017-11-22 23:50:21 +0300

    dima19972525
    Too many changed and i'm too lasy

---
+++ Fututun/Assets/Scenes/Game/Scripts/ManagerGameScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerGameScene : MonoBehaviour {
+
+    public Transform BackGround;
+    public BoxCollider2D Ground, Sky;
+
+    private const float wallHeight = 1.0f;
+
+    void Awake() {
+        // Подгоняем размеры фона, под размеры камеры
+        float v = Camera.main.orthographicSize * 2.0f;
+        float h = v * Screen.width / Screen.height;
+        BackGround.localScale = new Vector3(h, v, 1);
+
+        Ground.size = new Vector2(h, wallHeight);
+        Sky.size = Ground.size;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Fututun/Assets/Scenes/Main/Scripts/UIButtonsEvents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonsEvents : MonoBehaviour {
+
+    public void StartPressed()
+    {
+        Debug.Log("START");
+    }
+
+    public void ExitPressed()
+    {
+        Debug.Log("EXIT");
+    }
+}
